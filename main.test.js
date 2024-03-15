f15fb9806455f9ccabbe38510002c97b10de7fbb
Abida Sultana <abida.sultana9412@gmail.com>
2024-03-15 10:08:07 +0000

    5 tests added including error handling

--- main.test.js
+++ main.test.js
@@ -1,11 +1,29 @@
 import { test, expect } from "vitest";
 import { catTongue } from "./main.js";
 
-
+//bare bones test
 test('bare bones test should pass', () => {
-   
 })
+//actual tests
+test("upper case", function () {
+    expect(catTongue("MEOW").toBe("Hurry up!"));
+});
 
-// test("sample test", function () {
-//     expect(catTongue("meow").toBe("I want pets"));
-// });
+test("lowercase meow", function () {
+    expect(catTongue("meow").toBe("I want pets"));
+});
+
+test("5 characters or more", function () {
+    expect(catTongue("meowww").toBe("I'm hungry"));
+});
+
+test("string reversed", function () {
+    expect(catTongue("woem").toBe("too much catnip"));
+});
+
+//Error handling
+test("unsupported character", () => {
+    expect(() => catTongue("!")).toThrowError(
+      /^Unsupported character %$/
+    );
+  });
